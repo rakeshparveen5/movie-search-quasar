903253384e2cf35fc5bed2834162c914977a8301
rakeshparveen5 <rakesh.parveen5@gmail.com>
2026-03-16 13:35:15 +0530

    feature(movieList)- added loader while api loads
    When loadMovies async call is in progress the spinner is shown till the time

--- src/pages/MovieListPage.vue
+++ src/pages/MovieListPage.vue
@@ -6,13 +6,26 @@
 
     <q-btn label="Search" color="primary" class="q-mt-md" @click="loadMovies" />
 
-    <div class="row q-col-gutter-md q-mt-lg">
-      <div class="col-12" v-show="movies.length === 0">
-        <div class="text-subtitle1">No movies found. Try searching for something else.</div>
+    <div class="movie-grid-container">
+      <div class="row q-col-gutter-md q-mt-lg">
+        <div
+          v-show="!loading"
+          class="col-12 col-sm-6 col-md-3"
+          v-for="movie in movies"
+          :key="movie.imdbID"
+        >
+          <MovieCard :movie="movie" />
+        </div>
+        <div class="col-12" v-show="movies.length === 0 && !loading">
+          <div class="text-subtitle1">No movies found. Try searching for something else.</div>
+        </div>
       </div>
-      <div class="col-12 col-sm-6 col-md-3" v-for="movie in movies" :key="movie.imdbID">
-        <MovieCard :movie="movie" />
-      </div>
+      <q-inner-loading
+        :showing="loading"
+        label="Loading movies..."
+        label-class="text-primary"
+        size="50px"
+      />
     </div>
 
     <router-view v-slot="{ Component }">
@@ -37,14 +50,28 @@ const closeDialog = async () => {
   await router.push('/movie-list');
 };
 
+const loading = ref<boolean>(false);
 const search = ref<string>('');
 const movies = ref<Movie[]>([]);
 
 const loadMovies = async (): Promise<void> => {
-  const data = await searchMovies(search.value);
-
-  movies.value = data || [];
+  loading.value = true;
+  try {
+    const data = await searchMovies(search.value);
+    movies.value = data || [];
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+  } finally {
+    loading.value = false;
+  }
 };
 
 onMounted(loadMovies);
 </script>
+
+<style scoped>
+.movie-grid-container {
+  position: relative;
+  min-height: 300px;
+}
+</style>
